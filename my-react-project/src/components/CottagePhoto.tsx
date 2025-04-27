4a243abdcdf668cb49db01b0f80b0c4ff0e056f8
sofia-creates <sofia.ledin2@gmail.com>
2025-04-27 17:42:44 +0200

    starting on image selection

--- my-react-project/src/components/CottagePhoto.tsx
+++ my-react-project/src/components/CottagePhoto.tsx
@@ -2,6 +2,8 @@ import imagePath from "../assets/haunted-house-7508035_1280.jpg"
 import styles from "./cottagePhoto.module.scss"
 
 const CottagePhoto = () => {
+  //importera currentLevel
+  //let currentImagePath = ../assets/{level}
   return (
         <img  className={styles.cottagePhoto} src={imagePath} alt="Your cottage!" /> 
   )
