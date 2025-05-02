2f1b84782b9b96a319a2ed15614ff5c3f5a0cb4f
sofia-creates <sofia.ledin2@gmail.com>
2025-05-02 16:18:55 +0200

    imagePaths

--- my-react-project/src/components/CottagePhoto.tsx
+++ my-react-project/src/components/CottagePhoto.tsx
@@ -1,13 +1,13 @@
-import imagePath from "../assets/cottagePhoto-1.jpg"
+// import imagePath from "../assets/cottagePhoto-1.jpg"
 import styles from "./cottagePhoto.module.scss"
 
 type Props = { level: number };
 
 const CottagePhoto = ({ level }: Props) => {
   //importera Level från app?
-  //let currentImagePath = ../assets/cottagePhoto-{level}
+  let currentImagePath = "{../assets/cottagePhoto-{level}.jpg}"
   return (
-        <img  className={styles.cottagePhoto} src={imagePath} alt={`Your cottage, level ${level}`} /> 
+        <img  className={styles.cottagePhoto} src={currentImagePath} alt={`Your cottage, level ${level}`} /> 
   )
 }
 
