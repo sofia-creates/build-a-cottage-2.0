c916269f3b24846a5cb1719aa3189510b4e73739
sofia-creates <sofia.ledin2@gmail.com>
2025-05-08 21:49:04 +0200

    better typescript

--- my-react-project/src/components/CottagePhoto.tsx
+++ my-react-project/src/components/CottagePhoto.tsx
@@ -2,14 +2,13 @@ import photo1 from "../assets/cottagePhoto-1.jpg"
 import photo2 from "../assets/cottagePhoto-2.jpg"
 import styles from "./cottagePhoto.module.scss"
 
-type Props = { level: number };
 
 const imageMap: Record<number, string> = {
   1: photo1,
   2: photo2,
 };
 
-const CottagePhoto = ({ level }: Props) => {
+const CottagePhoto = ({ level }: { level: number }) => {
   let currentImagePath = imageMap[level]
   return (
         <img  className={styles.cottagePhoto} src={currentImagePath} alt={`Your cottage, level ${level}`} /> 
