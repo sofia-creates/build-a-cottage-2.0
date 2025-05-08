060020f7d13f75393ce4fa3ab51b027984778fdf
sofia-creates <sofia.ledin2@gmail.com>
2025-05-08 21:48:14 +0200

    image connection to level works

--- my-react-project/src/components/CottagePhoto.tsx
+++ my-react-project/src/components/CottagePhoto.tsx
@@ -2,12 +2,15 @@ import photo1 from "../assets/cottagePhoto-1.jpg"
 import photo2 from "../assets/cottagePhoto-2.jpg"
 import styles from "./cottagePhoto.module.scss"
 
-
-
 type Props = { level: number };
 
+const imageMap: Record<number, string> = {
+  1: photo1,
+  2: photo2,
+};
+
 const CottagePhoto = ({ level }: Props) => {
-  let currentImagePath = "{../assets/cottagePhoto-{level}.jpg}"
+  let currentImagePath = imageMap[level]
   return (
         <img  className={styles.cottagePhoto} src={currentImagePath} alt={`Your cottage, level ${level}`} /> 
   )
